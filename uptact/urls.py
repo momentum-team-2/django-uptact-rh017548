aad50bf1566bec2365d4184f3466acee41ce09d3
Rh017548 <russellhill752@msn.com>
2020-06-29 00:05:09 -0400

    uptact

--- uptact/urls.py
+++ uptact/urls.py
@@ -22,12 +22,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', contacts_views.list_contacts, name='list_contacts'),
     path('contacts/add/', contacts_views.add_contact, name='add_contact'),
+    path('contacts/<int:pk>/', contacts_views.show_contact, name='show_contact'),
     path('contacts/<int:pk>/edit/',
          contacts_views.edit_contact,
          name='edit_contact'),
     path('contacts/<int:pk>/delete/',
          contacts_views.delete_contact,
          name='delete_contact'),
+    path('contacts/<int:contact_pk>/notes',
+         contacts_views.add_note, name='add_note')
 ]
 
 if settings.DEBUG:
